1940d04f5be58d8acb895d95cd4d950984334bd4
Krishna Dasari <85394301+krishnadasari22@users.noreply.github.com>
2021-12-29 23:02:25 +0530

    first commit

---
+++ StudentManagementSystem/src/main/java/com/studentmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.studentmanagementsystem.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.sms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
